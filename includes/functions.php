57e9467c4d2d7510b1f1fad855d1e44fc2def6fe
stefan <pr.panescustefan@gmail.com>
2024-04-09 16:37:43 +0200

    added flushTables() - rebuilds the mysql tables empty

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'core.php';
+
 /**
  * @param $csvFile - path to csv to ingest
  * @param $limit - maximum number of rows to parse
@@ -34,3 +36,68 @@ function parseCSV($csvFile, $delimiter = ",", $limit = null, $return_assoc = fal
     return $csvData;
 }
 
+
+function flushTables()
+{
+    global $mysqli;
+
+    $mysqli->query("SET FOREIGN_KEY_CHECKS=0;");
+
+    //TABLE `global_keys`
+    $mysqli->query("DROP TABLE IF EXISTS `global_keys`");
+    $mysqli->query("
+    CREATE TABLE `global_keys` (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        global_key VARCHAR(64) NOT NULL,
+        global_key_label VARCHAR(128),
+        type INT,
+        weight INT
+    )");
+
+
+    //TABLE `data_sets`
+    $mysqli->query("DROP TABLE IF EXISTS `data_sets`");
+    $mysqli->query("
+    CREATE TABLE `data_sets` (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(64) NOT NULL
+    )");
+
+    //TABLE `templates`
+    $mysqli->query("DROP TABLE IF EXISTS `templates`");
+    $mysqli->query("
+    CREATE TABLE `templates` (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        data_set_id INT, FOREIGN KEY(data_set_id) REFERENCES data_sets(id),
+        local_key VARCHAR(64),
+        local_key_label VARCHAR(128),
+        global_key_id INT, FOREIGN KEY(global_key_id) REFERENCES global_keys(id)
+    )");
+
+
+
+    //TABLE `transactions`
+    $mysqli->query("DROP TABLE IF EXISTS `transactions`");
+    $mysqli->query("
+    CREATE TABLE `transactions` (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(64) NOT NULL,
+        template_id INT, FOREIGN KEY(template_id) REFERENCES templates(id),
+        d_insert TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        status INT
+    )");
+
+    //ingest queue
+    $mysqli->query("DROP TABLE IF EXISTS `ingest_queue`");
+    $mysqli->query("
+        CREATE TABLE `ingest_queue`(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            transaction_id INT, FOREIGN KEY(transaction_id) REFERENCES transactions(id),
+            entry_id INT,
+            template_id INT, FOREIGN KEY(template_id) REFERENCES templates(id),
+            entry_value TEXT,
+            status INT            
+        )
+    ");
+    $mysqli->query("SET FOREIGN_KEY_CHECKS=1;");
+}
--- main.php
+++ main.php
@@ -2,14 +2,17 @@
 
 require 'includes/functions.php';
 
-$arr = parseCSV("data/google_dataset.csv", ",", 100000, true);
-//$arr = parseCSV("data/facebook_dataset.csv", ",", 15, true);
-//$arr = parseCSV("data/website_dataset.csv", ";", 15, true);
 
-print_r(count($arr));
+flushTables();
 
-foreach($arr as $val)
-$test[$val['name']] = $val['name'];
-
-//print_r($test);
-echo "\n\n\n" . count($test);
+//$arr = parseCSV("data/google_dataset.csv", ",", 100000, true);
+////$arr = parseCSV("data/facebook_dataset.csv", ",", 15, true);
+////$arr = parseCSV("data/website_dataset.csv", ";", 15, true);
+//
+//print_r(count($arr));
+//
+//foreach($arr as $val)
+//$test[$val['name']] = $val['name'];
+//
+////print_r($test);
+//echo "\n\n\n" . count($test);
