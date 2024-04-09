81185a129b54df6a5f2098ba5ec8568b91c57cb7
Stefan <pr.panescustefan@gmail.com>
2024-04-09 07:48:52 +0300

    small changes to main script

--- main.php
+++ main.php
@@ -1,3 +1,15 @@
 <?php
 
-    echo "I'm alive";
+require 'includes/functions.php';
+
+$arr = parseCSV("data/google_dataset.csv", ",", 100000, true);
+//$arr = parseCSV("data/facebook_dataset.csv", ",", 15, true);
+//$arr = parseCSV("data/website_dataset.csv", ";", 15, true);
+
+print_r(count($arr));
+
+foreach($arr as $val)
+$test[$val['name']] = $val['name'];
+
+//print_r($test);
+echo "\n\n\n" . count($test);
